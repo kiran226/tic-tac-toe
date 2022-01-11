c944963d6d4af468c24870ca7d49a930e473da04
kiran <gandikotakiran99@gmail.com>
2022-01-11 17:27:40 +0530

    tic-tac-toe game

--- src/components/Icon.js
+++ src/components/Icon.js
@@ -1,21 +1,14 @@
 import React from 'react';
 import {FaTimes,FaPen,FaRegCircle} from 'react-icons/fa';
 const Icon = ({name}) =>{
-    return(
-        <div>
-            <h1>
-                <FaTimes className='icon' />
-            </h1>
-
-            <h1>
-                <FaPen className='icon' />
-            </h1>
-
-            <h1>
-                <FaRegCircle className='icon'/>
-            </h1>
-
-        </div>
-    )
+    switch(name){
+        case 'circle' :
+            return <FaRegCircle className='icon'/>
+        case 'cross' :
+            return <FaTimes className='icon'/>
+        default :
+            return <FaPen className='icon' />
+     }
 }
+    
 export default Icon;
